1e7947b94e73decd5325adce916869706787976a
user <user@DESKTOP-05SA252>
2021-05-03 11:00:55 -0500

    Proyecto departamental uso de .json

---
+++ flutter_application_1/lib/pages/home_page.dart
@@ -0,0 +1,79 @@
+import'package:flutter/cupertino.dart'; 
+import'package:flutter/material.dart'; 
+import'package:flutter_application_1/providers/menu_provider.dart'; 
+import'package:flutter_application_1/utils/icono_string_util.dart'; 
+
+class HomePage extends StatelessWidget { 
+ @override 
+
+Widget build(BuildContext context) { 
+return Scaffold( 
+appBar: AppBar( 
+  title:Text('Componetes Proyecto'), 
+), 
+  body:_lista(context), 
+); 
+  } 
+
+  Widget _lista(BuildContext context){ 
+    return FutureBuilder( 
+future:menuProvider.cargarData(), 
+initialData:[], 
+builder:(context,AsyncSnapshot<List<dynamic>>snapshot){ 
+return ListView( 
+children: _listaItems(snapshot.data,context), 
+  ); 
+}, 
+    ); 
+} 
+
+List<Widget> _listaItems(List<dynamic> data,BuildContext context) { 
+final List<Widget> opciones = []; 
+data.forEach((opt){ 
+final widgetTemp = ListTile( 
+title: Text(opt['texto']), 
+leading:getIcon(opt['icon']), 
+trailing:Icon( 
+Icons.keyboard_arrow_right_sharp, 
+color: Colors.lightBlueAccent, 
+    ), 
+onTap:(){ 
+showDialog( 
+context:context, 
+builder:(BuildContext context) =>_builPopUpDialog(context,opt['texto']), 
+); 
+  }, 
+    ); 
+opciones..add(widgetTemp)..add(Divider()); 
+
+}
+  ); 
+
+return opciones; 
+
+  } 
+
+Widget _builPopUpDialog(BuildContext context, String opt){ 
+return new AlertDialog( 
+title: const Text('Selecciono'), 
+content: new Column( 
+mainAxisSize:MainAxisSize.min, 
+children:<Widget>[ 
+Text('Apartado ' + opt), 
+], 
+), 
+
+actions:<Widget>[ 
+new FlatButton( 
+onPressed:(){ 
+Navigator.of(context).pop(); 
+}, 
+
+textColor:Theme.of(context).primaryColor, 
+child:const Text('Cerrar'), 
+    ), 
+  ], 
+      ); 
+} 
+
+} 
